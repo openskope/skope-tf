7c7529c009d7d7e58884a10d04a4c8076f511dbe
Timothy McPhillips <tmcphillips@absoluteflow.org>
2019-01-16 20:52:36 -0800

    added public and private subnets to staging vpc

--- aws/stage/resources.tf
+++ aws/stage/resources.tf
@@ -4,6 +4,27 @@ provider "aws" {
     secret_key = "${var.aws_secret_access_key}"
 }
 
-resource "aws_vpc" "skope_staging_vpc" {
-    cidr_block = "${var.skope_staging_vpc_address_space}"
+resource "aws_vpc" "staging_vpc" {
+    cidr_block = "${var.staging_vpc_address_space}"
+    tags = {
+        Name = "staging_vpc"
+    }
+}
+
+resource "aws_subnet" "staging_public_subnet" {
+    cidr_block      = "${var.staging_public_subnet_address_space}"
+    vpc_id          = "${aws_vpc.staging_vpc.id}"
+    map_public_ip_on_launch = "true"
+    tags = {
+        Name = "staging_public_subnet"
+    }
+}
+
+resource "aws_subnet" "staging_private_subnet" {
+    cidr_block      = "${var.staging_private_subnet_address_space}"
+    vpc_id          = "${aws_vpc.staging_vpc.id}"
+    map_public_ip_on_launch = "false"
+    tags = {
+        Name = "staging_private_subnet"
+    }
 }
--- aws/stage/variables.tf
+++ aws/stage/variables.tf
@@ -3,6 +3,14 @@
 variable "aws_access_key_id" {}
 variable "aws_secret_access_key" {}
 
-variable "skope_staging_vpc_address_space" {
+variable "staging_vpc_address_space" {
     default = "10.1.0.0/16"
+}
+
+variable "staging_public_subnet_address_space" {
+    default = "10.1.0.0/24"
+}
+
+variable "staging_private_subnet_address_space" {
+    default = "10.1.1.0/24"
 }
