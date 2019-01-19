77fefcb40915e09bfb85ea591a81fa0ddb299caa
Timothy McPhillips <tmcphillips@absoluteflow.org>
2019-01-18 18:13:58 -0800

    moved terraform files for s3 bucket into own directory

--- aws/global/s3-bucket-for-terraform-state/resources.tf
+++ aws/global/s3-bucket-for-terraform-state/resources.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         bucket="skope-aws-tf-state"
-        key="global/s3/terraform.tfstate"
+        key="global/s3-bucket-for-terraform-state/terraform.tfstate"
         region="us-west-1"
         encrypt="true"
         }
@@ -12,7 +12,7 @@ provider "aws" {
     access_key = "${var.access_key}"
     secret_key = "${var.secret_key}"
 }
-resource "aws_s3_bucket" "skope_aws_state" {  
+resource "aws_s3_bucket" "skope-aws-tf-state" {  
     bucket = "skope-aws-tf-state"  
     tags = {
         scope = "global"
